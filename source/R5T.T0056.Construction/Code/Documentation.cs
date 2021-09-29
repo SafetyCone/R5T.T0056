337eeb905965f8c2e81150c13d33367540d8c801
davidcoats <david@getsafetycone.com>
2021-09-29 14:13:24 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0056.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0056.Construction
+{
+    /// <summary>
+    /// Construction project for NamespacedTypeName and other related simple data types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0056/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0056
+{
+    /// <summary>
+    /// NamespacedTypeName and other related simple data types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
