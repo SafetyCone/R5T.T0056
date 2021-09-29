1c3b4ee7eda9fc77ebcc625b6a48800f8d8a6f05
David Coats <david@getsafetycone.com>
2021-09-29 14:15:14 -0700

    Add initial NamespacedTypeNameFilePath class.

---
+++ source/R5T.T0056/Code/Classes/NamespacedTypeNameFilePath.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+namespace R5T.T0056
+{
+    public class NamespacedTypeNameFilePath
+    {
+        public string NamespacedTypeName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
